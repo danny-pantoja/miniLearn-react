8041f631aca61b7db06086d756035b71cdf877fc
danny-pantoja <danny.pantoja.23@gmail.com>
2020-10-06 09:06:58 -0700

    commit to merge with master

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,9 +1,6 @@
 let apiUrl
 const apiUrls = {
   production: 'https://mini-learn-django.herokuapp.com/',
-=======
-  production: 'https://mini-learn-django.herokuapp.com/',
->>>>>>> a7928d43f454e87919365c6f5967438268b640cb
   development: 'http://localhost:8000'
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import './Home.css'
+// import Layout from '../shared/Layout/Layout'
+function LandingPage () {
+  // const landingPage = () => {}
+  return (
+    <Fragment>
+      const element = <h1>Hello, world</h1>
+    </Fragment>
+  )
+}
+
+export default LandingPage
